b38f00c027aa0bb4ac076b0f14bf3b7ef980d962
Taisho <marcaidajessmar@gmail.com>
2025-02-22 17:33:04 +0800

    Update AdminUserInfo.cs

--- Models/AdminUserInfo.cs
+++ Models/AdminUserInfo.cs
@@ -4,8 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using SQLite;
 
 namespace Barangay_Office.Models
@@ -13,7 +11,7 @@ namespace Barangay_Office.Models
     public class AdminUserInfo
     {
         [PrimaryKey]
-        public required string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Id { get; set; }
         public required string Username { get; set; }
         public required string Password { get; set; }
         public required string Role { get; set; } //super admin or customer

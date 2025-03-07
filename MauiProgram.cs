8ecad8d172311c9e877d52bb2f49bb664d627e6e
Taisho0 <marcaidajessmar@gmail.com>
2025-03-07 10:51:05 +0800

    Revert "yawa"
    This reverts commit 025883b9b541d1e119fd3481ecb9c1f5365786f2.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,10 +1,8 @@
-using Barangay_Office.Services;
+﻿using Barangay_Office.Services;
 using Barangay_Office.ViewModels;
 using Barangay_Office.Views;
 using Microsoft.Extensions.Logging;
 using SQLite;
-using UXDivers.Grial;
-using CommunityToolkit.Maui;
 
 namespace Barangay_Office
 {
@@ -19,20 +17,7 @@ namespace Barangay_Office
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("Poppins-Regular.ttf","Poppins");
-                    fonts.AddFont("materialdesignicons-webfont.ttf","Material Design Icons");
-                })
-
-
-                //for template
-                .UseGrial()
-                .ConfigureMauiHandlers(handlers =>
-                {
-                    handlers.AddHandler<NavigationPage, UXDivers.Grial.GrialNavigationPageHandler>();
-                    handlers.AddHandler<ScrollView, Barangay_Office.ScrollViewHandler>();
-                    handlers.AddHandler<Label, Barangay_Office.LabelHandler>();
-                })
-                .UseMauiCommunityToolkit();
+                });
 
 #if DEBUG
     		builder.Logging.AddDebug();

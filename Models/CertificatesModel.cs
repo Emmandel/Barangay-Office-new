979ee6d79d3336f30905169ccb34a44d0f5a54c1
Taisho0 <marcaidajessmar@gmail.com>
2025-05-06 12:36:58 +0800

    new update fixed all forms

---
+++ Models/CertificatesModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barangay_Office.Models
+{
+    public class CertificatesModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public Stream CertificateImage { get; set; } = Stream.Null;
+    }
+
+    public static class CertificateStorage
+    {
+        //centralized storage for certificates
+        public static ObservableCollection<CertificatesModel> Certificates { get; } = [];
+    }
+}
---
+++ Models/DynamicEntryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barangay_Office.Models
+{
+    public class DynamicEntryModel
+    {
+        public string Label { get; set; } = string.Empty;
+        public string PlaceHolder { get; set; } = string.Empty;
+        public bool IsVisible { get; set; }
+        public string BindingProperty { get; set; } = string.Empty;
+        public string InputType { get; set; } = string.Empty;
+    }
+}
